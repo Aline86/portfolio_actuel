5353fc3be2e291c4ab736057229473d6d5bfc0db
Claire-Aline Haestie <ca.haestie@gmail.com>
2025-06-29 09:58:57 +0200

    css improved

--- script.js
+++ script.js
@@ -1,55 +1,56 @@
-window.addEventListener("scroll", (e) => {
-  let rotation = window.scrollY * 0.1;
-  const leave_left = document.getElementById("leave_left");
-  const leave_right = document.getElementById("leave_right");
-  const shadow_right = document.querySelector(".shadow_right");
-  const container = document.querySelector(".container");
-  const name = document.querySelector(".name");
+document.addEventListener("DOMContentLoaded", () => {
+  window.addEventListener("scroll", (e) => {
+    let rotation = window.scrollY * 0.1;
+    const leave_left = document.getElementById("leave_left");
+    const leave_right = document.getElementById("leave_right");
+    const shadow_right = document.querySelector(".shadow_right");
+    const container = document.querySelector(".container");
+    const name = document.querySelector(".name");
 
-  const scrollY = window.scrollY || document.documentElement.scrollTop;
-  if (window.matchMedia("(max-width: 700px)").matches) {
-    if (scrollY > 150 && scrollY < 1000) {
-      container.classList.add("show");
+    const scrollY = window.scrollY || document.documentElement.scrollTop;
+    if (window.matchMedia("(max-width: 700px)").matches) {
+      if (scrollY > 150 && scrollY < 1000) {
+        container.classList.add("show");
+      } else {
+        container.classList.remove("show");
+      }
+    }
+    const portfolio = document.querySelector(".portfolio");
+    leave_left.style.transform = `translate3d(${-window.scrollY * 0.5}px, ${
+      -window.scrollY * 0.3
+    }px, ${-window.scrollY}px) rotateZ(-${rotation}deg)`;
+    leave_right.style.transform = `translate3d(${window.scrollY * 0.3}px, ${
+      -window.scrollY * 0.3
+    }px, ${-window.scrollY}px) rotateZ(${rotation}deg)`;
+    name.style.opacity = 0 + window.scrollY / 300;
+    shadow_right.style.transform = `translate3d(${-window.scrollY}px, ${
+      -window.scrollY * 0.5
+    }px, ${-window.scrollY * 2}px) rotateZ(-${rotation}deg)`;
+    portfolio.style.opacity = 0 + window.scrollY / 300;
+
+    if (window.scrollY <= 200) {
+      container.style.zIndex = -1;
     } else {
-      container.classList.remove("show");
+      container.style.zIndex = 9999;
     }
-  }
-  const portfolio = document.querySelector(".portfolio");
-  leave_left.style.transform = `translate3d(${-window.scrollY * 0.5}px, ${
-    -window.scrollY * 0.3
-  }px, ${-window.scrollY}px) rotateZ(-${rotation}deg)`;
-  leave_right.style.transform = `translate3d(${window.scrollY * 0.3}px, ${
-    -window.scrollY * 0.3
-  }px, ${-window.scrollY}px) rotateZ(${rotation}deg)`;
-  name.style.opacity = 0 + window.scrollY / 300;
-  shadow_right.style.transform = `translate3d(${-window.scrollY}px, ${
-    -window.scrollY * 0.5
-  }px, ${-window.scrollY * 2}px) rotateZ(-${rotation}deg)`;
-  portfolio.style.opacity = 0 + window.scrollY / 300;
 
-  if (window.scrollY <= 200) {
-    container.style.zIndex = -1;
-  } else {
-    container.style.zIndex = 9999;
-  }
-
-  if (window.matchMedia("(min-width: 700px)").matches) {
-    if (scrollY > 0) {
-      container.style.transform = `translate(-${scrollY}px, ${
-        scrollY * 0.5
-      }px) rotate(45deg)`;
+    if (window.matchMedia("(min-width: 700px)").matches) {
+      if (scrollY > 0) {
+        container.style.transform = `translate(-${scrollY}px, ${
+          scrollY * 0.5
+        }px) rotate(45deg)`;
+      }
+      if (
+        scrollY > window.innerHeight * 0.4 &&
+        scrollY < window.innerHeight * 0.8
+      ) {
+        container.classList.add("show");
+      } else {
+        container.classList.remove("show");
+      }
     }
-    if (
-      scrollY > window.innerHeight * 0.4 &&
-      scrollY < window.innerHeight * 0.8
-    ) {
-      container.classList.add("show");
-    } else {
-      container.classList.remove("show");
-    }
-  }
+  });
 });
-
 function show() {
   container_body = document.querySelector(".container_body");
   if (document.querySelector(".langues").classList.contains("show_langues")) {
