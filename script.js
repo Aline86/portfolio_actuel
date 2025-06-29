de4c8056c997e02aeebf540c5829237a09033e3b
Claire-Aline Haestie <ca.haestie@gmail.com>
2025-06-29 11:20:26 +0200

    css improved

--- script.js
+++ script.js
@@ -1,61 +1,20 @@
-window.addEventListener("scroll", (e) => {
-  let rotation = window.scrollY * 0.1;
-  const leave_left = document.getElementById("leave_left");
-  const leave_right = document.getElementById("leave_right");
-  const shadow_right = document.querySelector(".shadow_right");
-
-  const name = document.querySelector(".name");
-
-  const scrollY = window.scrollY || document.documentElement.scrollTop;
-
-  if (window.matchMedia("(min-width: 850px)").matches) {
-    const portfolio = document.querySelector(".portfolio");
-    leave_left.style.transform = `translate3d(${-window.scrollY * 0.5}px, ${
-      -window.scrollY * 0.3
-    }px, ${-window.scrollY}px) rotateZ(-${rotation}deg)`;
-    leave_right.style.transform = `translate3d(${window.scrollY * 0.3}px, ${
-      -window.scrollY * 0.3
-    }px, ${-window.scrollY}px) rotateZ(${rotation}deg)`;
-    name.style.opacity = 0 + window.scrollY / 300;
-    shadow_right.style.transform = `translate3d(${-window.scrollY}px, ${
-      -window.scrollY * 0.5
-    }px, ${-window.scrollY * 2}px) rotateZ(-${rotation}deg)`;
-    portfolio.style.opacity = 0 + window.scrollY / 300;
+window.addEventListener("DOMContentLoaded", () => {
+  if (window.matchMedia("(max-width: 850px)").matches) {
     const container = document.querySelector(".container");
-    if (window.scrollY <= 200) {
-      container.style.zIndex = -1;
-    } else {
-      container.style.zIndex = 9999;
-    }
-    if (scrollY > 0) {
-      container.style.transform = `translate(-${scrollY}px, ${
-        scrollY * 0.5
-      }px) rotate(45deg)`;
-    }
-    if (
-      scrollY > window.innerHeight * 0.4 &&
-      scrollY < window.innerHeight * 0.8
-    ) {
-      container.classList.add("show");
-    } else {
-      container.classList.remove("show");
-    }
-  } else {
-    const options = {
-      root: document.querySelector("body"),
-      rootMargin: "0px",
-      threshold: 0.8,
-    };
-    const observer = new IntersectionObserver(callback, options);
-    observer.observe(document.querySelector(".container_body"));
-    function callback(entries, observer) {
-      const leave_left = document.getElementById("leave_left");
-      const leave_right = document.getElementById("leave_right");
-      const shadow_right = document.querySelector(".shadow_right");
+    container.classList.add("show");
+    container.style.zIndex = 9999;
+  }
+  window.addEventListener("scroll", (e) => {
+    let rotation = window.scrollY * 0.1;
+    const leave_left = document.getElementById("leave_left");
+    const leave_right = document.getElementById("leave_right");
+    const shadow_right = document.querySelector(".shadow_right");
+    const portfolio = document.querySelector(".portfolio");
+    const name = document.querySelector(".name");
+    const container = document.querySelector(".container");
+    const scrollY = window.scrollY || document.documentElement.scrollTop;
 
-      const name = document.querySelector(".name");
-      const container = document.querySelector(".container");
-      const portfolio = document.querySelector(".portfolio");
+    if (window.matchMedia("(min-width: 850px)").matches) {
       leave_left.style.transform = `translate3d(${-window.scrollY * 0.5}px, ${
         -window.scrollY * 0.3
       }px, ${-window.scrollY}px) rotateZ(-${rotation}deg)`;
@@ -73,16 +32,33 @@ window.addEventListener("scroll", (e) => {
       } else {
         container.style.zIndex = 9999;
       }
-      entries.forEach((entry) => {
-        console.log(entry);
-        if (entry.isIntersecting) {
-          container.classList.add("show");
-        } else {
-          container.classList.remove("show");
-        }
-      });
+      if (scrollY > 0) {
+        container.style.transform = `translate(-${scrollY}px, ${
+          scrollY * 0.5
+        }px) rotate(45deg)`;
+      }
+      if (
+        scrollY > window.innerHeight * 0.4 &&
+        scrollY < window.innerHeight * 0.8
+      ) {
+        container.classList.add("show");
+      } else {
+        container.classList.remove("show");
+      }
+    } else {
+      leave_left.style.transform = `translate3d(${-window.scrollY * 0.5}px, ${
+        -window.scrollY * 0.3
+      }px, ${-window.scrollY}px) rotateZ(-${rotation}deg)`;
+      leave_right.style.transform = `translate3d(${window.scrollY * 0.3}px, ${
+        -window.scrollY * 0.3
+      }px, ${-window.scrollY}px) rotateZ(${rotation}deg)`;
+      name.style.opacity = 0 + window.scrollY / 300;
+      shadow_right.style.transform = `translate3d(${-window.scrollY}px, ${
+        -window.scrollY * 0.5
+      }px, ${-window.scrollY * 2}px) rotateZ(-${rotation}deg)`;
+      portfolio.style.opacity = 0 + window.scrollY / 300;
     }
-  }
+  });
 });
 
 function show() {
